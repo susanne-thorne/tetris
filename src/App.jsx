c749d7041a0e71ba188c8e785bc9620f72d28fef
Sargis Grigoryan <sargisgr@gmail.com>
2025-06-15 13:02:51 +0400

    Integrated SASS package

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,23 @@
-import "./App.css";
+import "./App.scss";
 
 function App() {
-  return <p>Tetris Comming Soon...</p>;
+  const width = 10;
+  const height = 20;
+  const board = Array(height)
+    .fill(null)
+    .map(() => Array(width).fill(null));
+
+  return (
+    <div className="container">
+      {board.map((row) => (
+        <div className="row">
+          {row.map((cell) => (
+            <div className="cell">a</div>
+          ))}
+        </div>
+      ))}
+    </div>
+  );
 }
 
 export default App;
